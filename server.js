66dba4ea3d692f7b7e19fc56bd3cf33c96f54101
Aaryan Mehta <aaryanmta@gmail.com>
2023-08-14 21:49:59 +0530

    connected to database

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
 
 app.get('/', (req, res) => {
-    res.send("Yo")
+    res.send("hello")
 })
 
 
-
-app.listen(3000, () => {
-    console.log("Node API app is running on port 3000")
+mongoose.connect('mongodb+srv://new_user:QHSycqHt3L5lagGg@apipractice.fz2rcqe.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+    app.listen(3000, () => {
+        console.log("Node API app is running on port 3000")
+    })
+    console.log("Connected to database")
+}).catch((error) => {
+    console.log(error)
 })
